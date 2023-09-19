96c954bd283dfa6321d1f6468a41e37ff4626f3c
setayeshnri <setayeshn1382@gmail.com>
2023-09-19 23:40:06 +0330

    Git URL added

---
+++ src/link.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Link() {
+  return (
+    <div>
+      <p>
+        <a href="https://github.com/setayeshnri/react-app.git">
+          open-sourced code
+        </a>
+        ,by Setayesh Nouri
+      </p>
+    </div>
+  );
+
+  
+}
